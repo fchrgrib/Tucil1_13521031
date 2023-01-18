8379bf8d0aed26d17670a1fd3598d3d4400e57fe
fchrgrib <fahrian.alifudin@gmail.com>
2023-01-18 21:19:07 +0700

    half progress

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Selamat datang di game 24 Solver!!!!!!!!!!!");
+        System.out.println("");
+
     }
 }
--- src/Tools.java
+++ src/Tools.java
@@ -1,5 +1,10 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Tools {
-    public String[] ifStarOrSlash(String[] nums){
+    private static final Scanner scan = new Scanner(System.in);
+    public static String[] ifStarOrSlash(String[] nums){
         for(int i = 0;i<nums.length;i++){
             if(nums[i].equals("*")){
                 nums[i+1]=String.valueOf(Double.parseDouble(nums[i-1])*Double.parseDouble(nums[i+1]));
@@ -13,7 +18,7 @@ public class Tools {
         }
         return nums;
     }
-    public double sumAll(String[] numsum){
+    public static double sumAll(String[] numsum){
         double sum = Double.parseDouble(numsum[0]);
         for(int i=0;i<numsum.length;i++){
             if(numsum[i].equals("+")){
@@ -25,4 +30,78 @@ public class Tools {
         return sum;
     }
 
+    public static boolean validationSave(){
+        System.out.println("Apakah anda ingin menyimpan data tersebut?\n1.Ya\n2.Tidak");
+        System.out.print("Pilih:");
+        int pilih = scan.nextInt();
+        while (pilih!=1||pilih!=2){
+            System.out.print("masukkan input yang valid: ");
+            pilih= scan.nextInt();
+        }
+        return pilih==1;
+    }
+
+    public static boolean validationNumber(int num1,int num2,int num3,int num4){
+        int[] num = {num1,num2,num3,num4};
+        for(int i=0;i<4;i++){
+            for (int j=0;j<4;j++){
+                if(i!=j){
+                    if(num[i]==num[j]){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void writeResults(String path,String[] result) throws IOException {
+        FileWriter writeResult = new FileWriter(path);
+        writeResult.write("result found: "+result.length);
+        if(result.length!=0){
+            for (int i=0;i<result.length;i++){
+                writeResult.write(result[i]+"\n");
+            }
+        }
+    }
+    public static int cardToNum(String card){
+        if(card=="A"){
+            return 1;
+        }
+        if(card=="J"){
+            return 11;
+        }
+        if(card=="Q"){
+            return 12;
+        }
+        if(card=="K"){
+            return 13;
+        }
+        return Integer.parseInt(card);
+    }
+    public static void ifInputKeyboardOrRandom(String result,boolean isKeyboard){
+        if(isKeyboard){
+            String[] card = new String[4];
+
+            System.out.println("Masukkan kartu: ");
+            for(int i=0;i<4;i++){
+                card[i]=scan.next();
+            }
+            int[] num = new int[4];
+            for(int i=0;i<4;i++){
+                num[i] = cardToNum(card[i]);
+            }
+            for(int i=0;i<4;i++){
+                for(int j=0;j<4;j++){
+                    for(int k=0;k<4;k++){
+                        for(int l=0;l<4;l++){
+                            if(i!=j&&i!=k&&i!=l&&j!=k&&j!=l&&l!=k){
+                                Results.results(result,num[i],num[j],num[k],num[l]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
