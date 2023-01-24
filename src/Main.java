231b5c02bb6d76feafb56c63dbeba53ab9cb53fd
fchrgrib <fahrian.alifudin@gmail.com>
2023-01-24 09:49:27 +0700

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Scanner;
 
 public class Main {
@@ -11,6 +13,7 @@ public class Main {
         while (!isEnd){
             String res = "";
             boolean isBoard = Tools.isKeyboard();
+            Instant stopwatch = Instant.now();
             String[] result = Tools.ifInputKeyboardOrRandom(res,isBoard);
             if(result[1]==""){
                 System.out.println("Tidak ada solusi\n\n");
@@ -18,7 +21,10 @@ public class Main {
                 String [] cobs = result[1].split("\n");
                 System.out.println(cobs.length+" Solutions found\n\n");
             }
+            Instant end = Instant.now();
+
             System.out.println(result[1]);
+            System.out.println("\n\nTime execuiton: "+Duration.between(stopwatch,end));
             boolean save= Tools.validationSave();
             if(save) Tools.writeResults(result[1],result[0]);
             isEnd = Tools.validationExit();
